574256c0a357afc6f62a0c66e5c0819ede334bc5
Ryan Dullaghan <ryan@nebula.ie>
2020-11-14 13:56:06 +0000

    Deck, Card, Player, and GameManager classes created. screen resolution cnaged to 1024 768

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public GameObject North, East, South, West;
+    CardType type;
+    int PlayerID;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch (type)
+        {
+            case CardType.Location:
+                Debug.Log("Do something");
+                break;
+            case CardType.Trap:
+                Debug.Log("Do something");
+                break;
+            case CardType.Equip:
+                Debug.Log("Do something");
+                break;
+            case CardType.Ritual:
+                Debug.Log("Do something");
+                break;
+            default:
+                Debug.Log("Error, card has no assigned type");
+                break;
+
+        }
+    }
+
+    public void Drawn(Player player)
+    {
+        PlayerID = player.PlayerID;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public enum CardType {Location, Trap, Equip, Ritual}
